d82e87d7dcdfd1dfe0d74f500d2cd53c591f32ec
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-08-27 15:43:39 -0600

    formatted, buttons still broke

--- app/models/Car.js
+++ app/models/Car.js
@@ -18,7 +18,7 @@ export default class Car {
             <h5 class="card-title">${this.year} - ${this.make} - ${this.model}</h5>
             <p class="card-text">${this.description}</p>
             <p><sm>${this.price}</sm></p>
-            <button class="btn btn-info" onclick="app.controllers.carCtrl.bid('${this._id}')">Bid</button>
+            <button class="btn btn-info" onclick="app.controllers.carController.bid('${this._id}')">Bid</button>
             <button class="btn btn-danger" onclick="app.controllers.carController.delete('${this._id}')">Delete Car</button>
         </div>
       </div>
