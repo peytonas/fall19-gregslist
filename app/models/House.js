bf0a45e4d8ed1e40900c1391d494548dccc45162
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-08-27 23:10:16 -0600

    fin except for job pics

--- app/models/House.js
+++ app/models/House.js
@@ -14,17 +14,17 @@ export default class House {
   get Template() {
     return `
       <div class="col-3">
-            <div class="card">
-                <img class="card-img-top" src="${this.imgUrl}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">${this.year} - ${this.location} - ${this.levels} - ${this.bathrooms} - ${this.bedrooms}</h5>
-                    <p class="card-text">${this.description}</p>
-                    <p><sm>${this.price}</sm></p>
-                    <button class="btn btn-info" onclick="app.controllers.houseController.bid('${this._id}')">Bid</button>
-                    <button class="btn btn-danger" onclick="app.controllers.houseController.delete('${this._id}')">Delete House</button>
-                </div >
-            </div >
-        </div >
-            `
+        <div class="card">
+          <img class="card-img-top" src="${this.imgUrl}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${this.year} - ${this.location} - ${this.levels} - ${this.bathrooms} - ${this.bedrooms}</h5>
+            <p class="card-text">${this.description}</p>
+            <p><sm>${this.price}</sm></p>
+            <button class="btn btn-info" onclick="app.controllers.houseController.bid('${this._id}')">Bid</button>
+            <button class="btn btn-danger" onclick="app.controllers.houseController.delete('${this._id}')">Delete House</button>
+        </div>
+      </div>
+    </div>
+    `
   }
 }
